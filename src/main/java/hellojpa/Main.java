be5d285c6599fc9d80a4cae7dc89369030044333
leedeoksoo <lds2292@naver.com>
2021-07-27 22:04:14 +0900

    first commit

---
+++ src/main/java/hellojpa/Main.java
@@ -0,0 +1,34 @@
+package hellojpa;
+
+import hellojpa.entity.Member;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class Main {
+    public static void main(String[] args) {
+        EntityManagerFactory emf =
+                Persistence.createEntityManagerFactory("hello");
+
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+            Member member = new Member();
+            member.setId(2L);
+            member.setName("라이언");
+            em.persist(member);
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
+    }
+}
